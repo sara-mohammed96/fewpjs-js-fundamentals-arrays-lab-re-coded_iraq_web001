02dc24448e35259c4134e5f91a565bd92fccbc22
sara altaee <sara.korea96@gmail.com>
2020-05-13 00:33:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const prepend=["Milo", "Otis", "Garfield"];
 const removeLast =["Milo", "Otis", "Garfield"];
 const removeFirst=["Milo", "Otis", "Garfield"];
 append.push("Odie");
-
+prepend.unshift("dog");
 removeLast.pop(removeLast.length-1);
 removeFirst.shift();
