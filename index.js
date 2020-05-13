b3bfeb8e96133a50357867a121370721dcfe0456
sara altaee <sara.korea96@gmail.com>
2020-05-13 00:15:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your solution here!
+
+const append=["Milo", "Otis", "Garfield"];
+const prepend=["Milo", "Otis", "Garfield"];
+const removeLast =["Milo", "Otis", "Garfield"];
+const removeFirst=["Milo", "Otis", "Garfield"];
+append.push("Odie");
+
+removeLast.pop(removeLast.length-1);
+removeFirst.unshift("Milo");
